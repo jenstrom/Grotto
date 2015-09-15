ae07ad32361fc93e6513fbf27e35dbbe60f835a1
Jonatan Enström <jonatan.enstrom@gmail.com>
2015-09-15 13:55:09 +0200

    added prefabs, materials, scripts

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,132 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour {
+
+	public bool MakingChoice {
+		get;
+		set;
+	}
+
+	//1 = wait
+	//2 = move
+	//3 = attack
+	//4 = ability
+	public int ActionToTake {
+		get;
+		set;
+	}
+
+	public Vector3 StartPosition {
+		get;
+		set;
+	}
+
+	public Vector3 MoveTo {
+		get;
+		set;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		MakingChoice = false;
+		ActionToTake = 0;
+		StartPosition = gameObject.transform.position;
+		MoveTo = StartPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	}
+
+	public void MakeChoice()
+	{
+			float choice = Random.value;
+			
+			if (choice < 0.25f)
+			{
+				Move("left");
+			}
+			else if (choice < 0.50f)
+			{
+				Move("right");
+			}
+			else if (choice < 0.75f)
+			{
+				Move("up");
+			}
+			else if (choice < 1f)
+			{
+				Move("down");
+			}
+		
+	}
+
+	void Move(string direction)
+	{
+		Vector3 Current = gameObject.transform.position;
+		StartPosition = Current;
+
+		switch (direction)
+		{
+			case "left":
+				MoveTo = new Vector3(Current.x -1, Current.y, Current.z);
+				break;
+
+			case "right":
+				MoveTo = new Vector3(Current.x +1, Current.y, Current.z);
+				break;
+
+			case "up":
+				MoveTo = new Vector3(Current.x, Current.y +1, Current.z);
+				break;
+
+			case "down":
+				MoveTo = new Vector3(Current.x, Current.y -1, Current.z);
+				break;
+		}
+
+		Collider[] colliders = Physics.OverlapSphere(MoveTo,0f);
+		
+		if (colliders.Length > 0)
+		{
+			MoveTo = StartPosition;
+
+			if (colliders[0].gameObject.tag == "Player")
+			{
+				Debug.Log("Rawr!");
+			}
+			else
+			{
+				bool[] validMoves = new bool[4];
+				validMoves[0] = Physics.OverlapSphere(new Vector3(Current.x -1, Current.y, Current.z),0f).Length > 0;
+				validMoves[1] = Physics.OverlapSphere(new Vector3(Current.x +1, Current.y, Current.z),0f).Length > 0;
+				validMoves[2] = Physics.OverlapSphere(new Vector3(Current.x, Current.y -1, Current.z),0f).Length > 0;
+				validMoves[3] = Physics.OverlapSphere(new Vector3(Current.x, Current.y +1, Current.z),0f).Length > 0;
+
+				for (int i = 0; i < validMoves.Length; i++) 
+				{
+					if (!validMoves[i])
+					{
+						MakeChoice();
+						break;
+					}
+				}
+			}
+		}
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,252 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayerController : MonoBehaviour 
+{
+	//public GameObject playerCollider;
+
+	public GameObject MoveTo {
+		get;
+		set;
+	}
+
+	public Vector3 StartPosition {
+		get;
+		set;
+	}
+
+	public bool PlayerInputAllowed {
+		get;
+		set;
+	}
+
+	public bool ActionTaken {
+		get;
+		set;
+	}
+
+	//1 = wait
+	//2 = move
+	//3 = attack
+	//4 = ability
+	public int ActionToTake {
+		get;
+		set;
+	}
+
+	void Start () 
+	{
+		ActionToTake = 0;
+		ActionTaken = false;
+		StartPosition = gameObject.transform.position;
+		MoveTo = new GameObject();
+	}
+
+	void Update () 
+	{
+		if (PlayerInputAllowed)
+		{
+			if (Input.GetButton("Horizontal") || Input.GetButton("Vertical") || Input.GetButton("Jump"))
+			{
+				PlayerInput();
+
+				/*
+				if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+				{
+					if (Input.GetAxis("Horizontal") < 0)
+					{
+						MoveTo = new Vector3(Current.x -1, Current.y, Current.z);
+					}
+					
+					if (Input.GetAxis("Horizontal") > 0)
+					{
+						MoveTo = new Vector3(Current.x +1, Current.y, Current.z);
+					}
+					
+					if (Input.GetAxis("Vertical") < 0)
+					{
+						MoveTo = new Vector3(Current.x, Current.y -1, Current.z);
+					}
+					
+					if (Input.GetAxis("Vertical") > 0)
+					{
+						MoveTo = new Vector3(Current.x, Current.y +1, Current.z);
+					}
+
+				}
+
+				PlayerInputAllowed = false;
+				Collider[] colliders = Physics.OverlapSphere(MoveTo,0.4f);
+				bool occupied = false;
+
+				for (int i = 0; i < colliders.Length; i++) {
+					if (!colliders[i].CompareTag("Floor"))
+					{
+						Debug.Log(colliders[i].tag);
+						MoveTo = Current;
+						occupied = true;
+					}
+				}
+
+				if (!occupied)
+				{
+					ActionToTake = 2;
+					ActionTaken = true;
+					MoveToObject = (GameObject) Instantiate(moveToCollider, MoveTo, Quaternion.identity);
+					Debug.Log(MoveToObject.transform.position);
+				}
+				*/
+			}
+		}
+		/*
+		if (gameObject.transform.position != MoveTo)
+			{
+			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, MoveTo, 0.1f);
+			}
+			else
+			{
+			current = MoveTo;
+				PerformingAction = false;
+			}
+
+		/*
+
+		if (Input.GetMouseButtonUp(0))
+		{
+			current = gameObject.transform.position;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			Physics.Raycast(ray, out hit);
+
+			moveQueue = new Vector3[] {hit.point};
+
+			//PlayerMove(moveQueue);
+		}
+
+		if (Input.GetButtonUp("Horizontal") || Input.GetButtonUp("Vertical"))
+		{
+			moveQueue = new Vector3[] {GetKeyboardMove(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"))};
+		}
+		//PlayerMove(moveQueue);
+		if (gameObject.transform.position != moveQueue[0])
+		{
+			gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, moveQueue[0], 1f);
+		}
+
+		Debug.Log(PlayerInput);
+	}
+
+
+	void PlayerMove(Vector3[] moveQueue)
+	{
+		for(int i = 0; i < moveQueue.Length; i++)
+		{
+				gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, moveQueue[i], 1f);
+				//gameObject.transform.position = moveQueue[i];
+		}
+		*/
+	}
+
+	void PlayerInput()
+	{
+		Vector3 current = gameObject.transform.position;
+
+		if (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))
+		{
+			if (Input.GetAxis("Horizontal") < 0)
+			{
+				PlayerAction(new Vector3(current.x -1, current.y, current.z));
+			}
+			
+			if (Input.GetAxis("Horizontal") > 0)
+			{
+				PlayerAction(new Vector3(current.x +1, current.y, current.z));
+			}
+			
+			if (Input.GetAxis("Vertical") < 0)
+			{
+				PlayerAction(new Vector3(current.x, current.y -1, current.z));
+			}
+			
+			if (Input.GetAxis("Vertical") > 0)
+			{
+				PlayerAction(new Vector3(current.x, current.y +1, current.z));
+			}	
+		}
+
+		if (Input.GetButton("Jump"))
+		{
+			PlayerInputAllowed = false;
+			ActionToTake = 1;
+			ActionTaken = true;
+			Debug.Log("ZzZz");
+		}
+	}
+	
+	void PlayerAction(Vector3 target)
+	{
+		Collider[] onTargetTile = Physics.OverlapSphere(target, 0f);
+
+		if (onTargetTile.Length == 0)
+		{
+			PlayerInputAllowed = false;
+			StartPosition = gameObject.transform.position;
+
+			MoveTo.SetActive(false);
+			Destroy(MoveTo);
+			MoveTo = (GameObject) Instantiate(new GameObject(), target, Quaternion.identity);
+			MoveTo.tag = "Player";
+			MoveTo.AddComponent<BoxCollider>();
+
+			ActionToTake = 2;
+			ActionTaken = true;
+		}
+		else if (onTargetTile[0].tag == "Enemy")
+		{
+			PlayerInputAllowed = false;
+			ActionToTake = 3;
+			ActionTaken = true;
+			Debug.Log("To Battle!");
+		}
+		else
+		{
+			Debug.Log("Ouch!");
+		}
+	}
+	
+	/*
+	Vector3 GetKeyboardMove (float horizontal, float vertical)
+	{
+		Vector3 targetDestination = Current;
+
+		if (horizontal < 0)
+		{
+			targetDestination = new Vector3(Current.x -1, Current.y, Current.z);
+		}
+		
+		if (horizontal > 0)
+		{
+			targetDestination = new Vector3(Current.x +1, Current.y, Current.z);
+		}
+
+		if (vertical < 0)
+		{
+			targetDestination = new Vector3(Current.x, Current.y, Current.z -1);
+		}
+		
+		if (vertical > 0)
+		{
+			targetDestination = new Vector3(Current.x, Current.y, Current.z +1);
+		}
+
+		if (Physics.OverlapSphere(targetDestination,0).Length > 0)
+		{
+			targetDestination = Current;
+		}
+		return targetDestination;
+		
+	}
+	*/
+	
+}
